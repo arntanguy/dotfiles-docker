ced209cbd741de5a4da0c810379ed4bae9203161
Arnaud TANGUY <arn.tanguy@gmail.com>
2015-11-16 14:25:32 +0900

    Use tmuxcopycat + sensible ycm config
    Tmux:
    - Install copycat plugin
    - Disable Space/BSpace mappings for copycat compatibility

--- dev/ycm_extra_conf.py
+++ dev/ycm_extra_conf.py
@@ -56,38 +56,16 @@ flags = [
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
 'c++',
-'-isystem',
-'../BoostParts',
-'-isystem',
-# This path will only work on OS X, but extra paths that don't exist are not
-# harmful
-'/System/Library/Frameworks/Python.framework/Headers',
-'-isystem',
-'../llvm/include',
-'-isystem',
-'../llvm/tools/clang/include',
 '-I',
 '.',
-'-I',
-'./ClangCompleter',
-'-isystem',
-'./tests/gmock/gtest',
-'-isystem',
-'./tests/gmock/gtest/include',
-'-isystem',
-'./tests/gmock',
-'-isystem',
-'./tests/gmock/include',
 '-isystem',
 '/usr/include',
 '-isystem',
 '/usr/local/include',
-'-isystem',
-'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../include/c++/v1',
-'-isystem',
-'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
 '-I',
-'/opt/ros/indigo/include'
+'/opt/ros/indigo/include',
+'-I',
+'/usr/include/eigen3'
 ]
 
 

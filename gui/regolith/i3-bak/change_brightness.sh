393348a3378eb973b6bca73584bc2c3f69ad5a49
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-15 11:12:59 +0200

    properly  remove submodule

--- gui/regolith/i3-bak/change_brightness.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo $((`cat /sys/class/backlight/intel_backlight/brightness`+${1:-100})) | tee /sys/class/backlight/intel_backlight/brightness
--- gui/regolith/i3-bak/i3GnomeSettings.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-export XDG_CURRENT_DESKTOP=ubuntu:GNOME; gnome-control-center
